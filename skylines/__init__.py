15b8f161f75f376063b74caa1ff2adf8f939980b
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-06-26 12:41:54 +0200

    Use SQLAlchemy from flask

--- skylines/__init__.py
+++ skylines/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from skylines.model import init_model
 
 app = Flask(__name__)
 app.config.from_object('skylines.default_settings')
 app.config.from_envvar('SKYLINES_SETTINGS', silent=True)
+
+init_model(create_engine(app.config['SQLALCHEMY_DATABASE_URI']))
 
 import skylines.views
--- skylines/views.py
+++ skylines/views.py
@@ -1,6 +1,12 @@
 from skylines import app
-
+from skylines.model import DBSession, Flight
+from pprint import pformat
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    q = DBSession.query(Flight)
+    l = []
+    for i in q:
+        l.append(i.filename)
+
+    return '<pre>' + pformat(l) + '</pre>'
